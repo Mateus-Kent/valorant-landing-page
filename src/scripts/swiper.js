cdb1d25c5c84c42db6a13673dfce17b574c3d37a
mateus-kent <mateusquent2003@gmail.com>
2022-09-15 23:35:31 -0300

    refactor: create file just for the swiper

---
+++ src/scripts/swiper.js
@@ -0,0 +1,29 @@
+new Swiper('.mapsSwiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 24,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+new Swiper('.agentsSwiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 45,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  mousewheel: true,
+  keyboard: true
+})
